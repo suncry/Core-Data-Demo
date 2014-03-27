c803239fd4923137f52f51da4e2352a893e81f33
Cy <774164@qq.com>
2014-03-27 14:06:43 +0800

    Initial Commit

---
+++ dataTest/CyAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CyAppDelegate.h
+//  dataTest
+//
+//  Created by niko on 14-3-27.
+//  Copyright (c) 2014年 Cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ dataTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dataTest
+//
+//  Created by niko on 14-3-27.
+//  Copyright (c) 2014年 Cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CyAppDelegate class]));
+    }
+}
